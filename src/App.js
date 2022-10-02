2bf2a98a8a21ab0d98a1db7ff8502fc756b2c902
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-02 15:04:04 +0200

    Added section classes and temporary bg-colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css"
 import Home from "./pages/Home.js"
+import About from "./pages/About.js"
+import Projects from "./pages/Projects.js"
 
 function App() {
   return <div className="App">
     <Home />
+    <About />
+    <Projects />
   </div>
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Home</div>
+  return <section className="section--home">HOME</section>
 }
