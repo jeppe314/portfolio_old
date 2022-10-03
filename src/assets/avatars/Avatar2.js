58d8e090bff98a8d217f332984c9b7ec664c259e
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-03 20:44:57 +0200

    Dabbling around with svgs

---
+++ src/assets/avatars/Avatar2.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Avatar_crouching from "./Avatar_crouching.svg"
+
+export default function Avatar2() {
+  return <img src={Avatar_crouching} alt="avatar crouching" />
+}
