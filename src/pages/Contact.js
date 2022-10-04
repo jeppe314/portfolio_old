80671fec4b253634166973e646584c59fff74702
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-04 07:37:10 +0200

    Addad contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Title from "../components/Title"
+
+export default function Contact() {
+  return (
+    <section className="section--contact">
+      <Title title="3. reach out" />
+    </section>
+  )
+}
